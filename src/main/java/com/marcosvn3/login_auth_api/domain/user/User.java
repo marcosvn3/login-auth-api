2e564ed10c67cf2bf248aca2c2a0827f2b14f854
Marcos Santos <marcosvn3@academico.ufs.br>
2024-08-25 15:11:56 -0300

    Criando entidade de usuario.

---
+++ src/main/java/com/marcosvn3/login_auth_api/domain/user/User.java
@@ -0,0 +1,25 @@
+package com.marcosvn3.login_auth_api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//modelo de usuario da aplicação
+@Entity
+@Table(name= "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String username;
+    private String password;
+    private String email;
+
+}
