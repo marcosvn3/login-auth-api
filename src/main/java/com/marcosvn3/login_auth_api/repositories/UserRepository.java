5f55e24fcfe02385ab9b46bc60b4b7af54e06cbd
Marcos Santos <marcosvn3@academico.ufs.br>
2024-08-25 15:53:47 -0300

    Adicionando metodo findByEmail.

--- src/main/java/com/marcosvn3/login_auth_api/repositories/UserRepository.java
+++ src/main/java/com/marcosvn3/login_auth_api/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package com.marcosvn3.login_auth_api.repositories;
 import com.marcosvn3.login_auth_api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 //Para declarar os repositories usando JPA basta ser uma interface
 public interface UserRepository extends JpaRepository<User,String> {
-
+    Optional<User> findByEmail(String email);
 }
